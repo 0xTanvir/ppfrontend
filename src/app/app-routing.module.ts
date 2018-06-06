dd4c08a05d7ba6a4d0607c6e6fbd2b490beff73e
Tanvir Rahman <tanvir3080@gmail.com>
2018-06-07 04:31:29 +0600

    Simple Contest Card concept added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
+import { ContestComponent } from './contest/contest.component';
 
 const routes: Routes = [
     {path: '', component: WelcomeComponent},
     {path: 'signup', component: SignupComponent},
     {path: 'login', component: LoginComponent},
+    {path: 'contest', component: ContestComponent},
 ];
 
 @NgModule({
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,7 +7,8 @@ import {
     MatCheckboxModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
 } from '@angular/material';
 
 @NgModule({
@@ -19,7 +20,8 @@ import {
         MatCheckboxModule,
         MatSidenavModule,
         MatToolbarModule,
-        MatListModule
+        MatListModule,
+        MatCardModule
     ],
     exports: [
         MatButtonModule,
@@ -29,7 +31,8 @@ import {
         MatCheckboxModule,
         MatSidenavModule,
         MatToolbarModule,
-        MatListModule
+        MatListModule,
+        MatCardModule
     ]
 })
 export class MaterialModule {}
